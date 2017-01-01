188c34e02962049a4f69bf7869709de444c92810
Taehwan Kim <maxtortime@gmail.com>
2017-01-01 19:35:37 +0900

    Can transfer post and reply to GitHub
    Writing README how to install and purpose of this plugin

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # discourse-topic-to-github
+
+This plugin can transfer discourse post and reply to 
+Github issue. When you write topic or reply, this plugin
+sends your post to GitHub repository automatically.
+But now it cannot detect modifying posts.
+
+## Installing
+1. **SHOULD READ THIS ARTICLE** https://meta.discourse.org/t/install-a-plugin/19157
+2. This plugin uses [octokit](https://github.com/octokit/octokit.rb). 
+Add this `gem "octokit", "~> 4.0"` line to `Gemfile` at discourse root directory.
+3. Run `bundle install` at terminal.
+
+## LICENSE
+MIT
+
--- plugin.rb
+++ plugin.rb
@@ -1,41 +1,47 @@
-# name: basic-plugin
-# about: A super simple plugin to demonstrate how plugins work
+# name: discourse-topic-to-github
+# about: This discourse plugin can copy topic and reply to GitHub issues
 # version: 0.0.1
 # authors: Taehwan Kim
 
+require 'octokit'
+
+enabled_site_setting :topic_transfer_enabled
+
+file_name = "./topic_by_issue.json"
+
 after_initialize do
+  if File.exists?(file_name)
+    file = File.read(file_name, "r")
+    topic_by_issue = JSON.parse(file.read)
+  else
+    topic_by_issue = Hash.new
+  end
+
   DiscourseEvent.on(:post_created) do |post_ids|
     post_id = post_ids[:id]
     post = Post.find_by({id: post_id})
-
-=begin
-TODO:
-# is_first_post == true
-1. post이므로 곧바로 issue를 create 한다.
-2. post가 만들어지고 issue가 create 될때 topic_id를 key로 하고 issue number를
-value로 해서 가지고 있는다.
-3. https://developer.github.com/v3/issues/#create-an-issue
-보내야할 json 형식
-{
-  "title": post.title
-  "body": post.raw # 어떻게 될지 모르겠지만 일단 raw를 보내보자
-}
-4. 보내고 난 후 response로 날아오는 number를 잘 챙겨서 2번을 수행한다
-
-# is_first_post == false
-1. reply이므로 위에 2번에서 만든 dict 에서 topic_id 를 이용해 issue number를 찾는다
-2. https://developer.github.com/v3/issues/comments/#create-a-comment
-3. 해당 issue number 에 아래 json 형식대로 전송한다
-{
-  "body": post.raw
-}
-=end
-
     topic = post.topic
+    access_token = SiteSetting.github_access_token
+    repo_name = SiteSetting.github_repo_name
 
-    puts "maxtortime #{topic.title}"
-    puts "maxtortime #{post.is_first_post?}, #{post.topic_id}"
-    puts "maxtortime #{post_id}: #{post.raw}"
+    if SiteSetting.github_enterprise_enabled
+      Octokit.configure do |c|
+        c.api_endpoint = "#{SiteSetting.github_enterprise_hostname}/api/v3/"
+      end
+    end
 
+    client = Octokit::Client.new(:access_token => access_token)
+
+    if post.is_first_post?
+      issue = client.create_issue(repo_name, title=topic.title, body=post.raw)
+      topic_by_issue[post.topic_id] = issue[:number]
+    else
+      issue_num = topic_by_issue[post.topic_id]
+      client.add_comment(repo_name, number=issue_num, comment=post.raw)
+    end
+
+    File.open(file_name, "w") do |f|
+      f.write(topic_by_issue.to_json)
+    end
   end
 end
